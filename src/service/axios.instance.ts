7c46e310459a98585e01a99cc70f95857a888c7e
Sanchir01 <emgushovs@mail.ru>
2023-11-15 07:48:46 +0300

    delete cookie

--- src/service/axios.instance.ts
+++ src/service/axios.instance.ts
@@ -1,16 +1,12 @@
 import axios from 'axios'
-import { cookies } from 'next/headers'
-import { EnumTokens } from './auth.service'
 
 export const instance = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 	method: 'POST'
 })
-const token = cookies().get(EnumTokens.ACCESS_TOKEN)?.value
 
 const headers = {
-	'content-type': 'application/json',
-	Authorization: token ? `Bearer ${token}` : ''
+	'content-type': 'application/json'
 }
 const graphqlQuery = {
 	query: `query GetAllProductsDashboard($getAllProductInput: GetAllProductInput!) {
